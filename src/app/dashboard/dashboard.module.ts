be1b929064858708f5a380b9a638a6d987c84f0d
Davis Ortega <Davisoortega@gmail.com>
2023-10-23 20:37:43 -0500

    first commit

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,21 +5,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsComponent } from './forms/forms.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { UsersComponent } from './pages/users/users.component';
+import { MatTableModule } from '@angular/material/table';
+import { UsersComponent } from './users/users.component';
 import { MatDialogModule } from '@angular/material/dialog';
+
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    FormsComponent,
+
     UsersComponent
+
   ],
   imports: [
     CommonModule,
@@ -32,6 +34,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatInputModule,
     ReactiveFormsModule,
     MatDialogModule,
+    MatTableModule
 
 
 
